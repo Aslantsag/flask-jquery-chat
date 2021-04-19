26da315f1e81952ca04dbb5196eb1bef5f77d1ae
Your Name <you@example.com>
2021-04-19 16:34:11 +0300

    Upload

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-app.secret_key = 'X38cXdx10Sd8Xx'
+app.secret_key = '<SECRET KEY>'
 
 
 class Channel(db.Model):
